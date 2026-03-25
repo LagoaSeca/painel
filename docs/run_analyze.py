17b6dbe20dcba045172ba82f804c6ec69ecf56b7
Wlademir <wlademir49@yahoo.com.br>
2026-03-24 21:22:24 -0300

    remove .env asset

--- docs/run_analyze.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-out = subprocess.run(
-    ["dart", "analyze", "lib/core/network/manager_api_service.dart", "lib/features/flyer/presentation/pages/tela_gerenciar_flyers.dart"], 
-    capture_output=True, 
-    text=True
-)
-print("STDOUT:", out.stdout)
-print("STDERR:", out.stderr)
